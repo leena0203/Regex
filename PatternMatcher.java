fab3fca51160769aadfb7e70619dd7238b1c7f29
leena0203 <leenasarode02@gmail.com>
2020-09-30 02:57:14 +0530

    UC1FirstName commited

--- PatternMatcher.java
+++ PatternMatcher.java
@@ -1,11 +1,27 @@
 package Regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PatternMatcher {
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		PatternMatcher patternMatcher = new PatternMatcher();
+		String samplel = "Leena";
+		System.out.println(samplel+"------->"+patternMatcher.validateInput(samplel));
+		String samplel1 = "eela23";
+		System.out.println(samplel1+"------->"+patternMatcher.validateInput(samplel1));
 	}
-}
-
-
+	
+	      
+	      private String validateInput(String samplel) {
+	    	  String regex = "^[A-Z a-z]{3,25}$";
+	    	  Pattern pattern = Pattern.compile(regex);
+	    	  Matcher matcher = pattern.matcher(samplel);
+	    	  if(matcher.find()) {
+	    		  return "valid";
+	    		  
+	    	  }
+	    	  return "invalid";
+	      }
+	}
 
