8f54c9a4dce108c9d8aa712041e4542e0d52854d
leena0203 <leenasarode02@gmail.com>
2020-09-30 03:11:27 +0530

    UC3EmailId commited

--- PatternMatcher.java
+++ PatternMatcher.java
@@ -10,11 +10,23 @@ public class PatternMatcher {
 		System.out.println("First Name of User:"+samplel+"------->"+patternMatcher.validateInput(samplel));
 		String samplel1 = "Sarode";
 		System.out.println("Last Name of User:"+samplel1+"------->"+patternMatcher.validateInput(samplel1));
+		String email = "abc.LYX@bl.co.com";
+		System.out.println("EmailId of User:"+email+"------->"+patternMatcher.validateEmail(email));
 	}
 	
 	      
 	      private String validateInput(String samplel) {
 	    	  String regex = "^[A-Z a-z]{3,25}$";
+	    	  Pattern pattern = Pattern.compile(regex);
+	    	  Matcher matcher = pattern.matcher(samplel);
+	    	  if(matcher.find()) {
+	    		  return "valid";
+	    		  
+	    	  }
+	    	  return "invalid";
+	      }
+	      private String validateEmail(String samplel) {
+	    	  String regex = "^(abc[.][A-Za-z]+@bl[.]co[.][A-Za-z]{3,25})$";
 	    	  Pattern pattern = Pattern.compile(regex);
 	    	  Matcher matcher = pattern.matcher(samplel);
 	    	  if(matcher.find()) {
