12dd501e87a96f9a9c94c5b9f9e605fe5f9bd69e
leena0203 <leenasarode02@gmail.com>
2020-10-01 01:58:02 +0530

    UC9EmailAllSample commited

--- PatternMatcher.java
+++ PatternMatcher.java
@@ -10,13 +10,39 @@ public class PatternMatcher {
 		System.out.println("First Name of User:"+samplel+"------->"+patternMatcher.validateInput(samplel));
 		String samplel1 = "Sarode";
 		System.out.println("Last Name of User:"+samplel1+"------->"+patternMatcher.validateInput(samplel1));
-		String email = "abc.LYX@bl.co.com";
-		System.out.println("EmailId of User:"+email+"------->"+patternMatcher.validateEmail(email));
 		String Number = "91 2345678012";
 		System.out.println("Phone NO. of User:"+Number+"------->"+patternMatcher.validateMobileNum(Number));
-		String Password = "Soniakh@45";
+		String Password = "Soni#akh@45";
 		System.out.println("Password of User:"+Password+"------->"+patternMatcher.validatePassword(Password));
+		String email = "abc@gmail.com";
+		System.out.println("EmailId of User:"+email+"------->"+patternMatcher.validateEmail(email));
+		String email1 = "abc-100@yahoo.com";
+		System.out.println("EmailId of User:"+email1+"------->"+patternMatcher.validateEmail(email1));
+		String email2 = "abc.100@yahoo.com";
+		System.out.println("EmailId of User:"+email2+"------->"+patternMatcher.validateEmail(email2));
+		String email3 = "abc-100@abc.net";
+		System.out.println("EmailId of User:"+email3+"------->"+patternMatcher.validateEmail(email3));
+		String email4 = "abc.100@abc.com.au";
+		System.out.println("EmailId of User:"+email4+"------->"+patternMatcher.validateEmail(email4));
+		String email5 = "abc@gmail.com.com";
+		System.out.println("EmailId of User:"+email5+"------->"+patternMatcher.validateEmail(email5));
+		String email6 = "abc@.com.my";
+		System.out.println("EmailId of User:"+email6+"------->"+patternMatcher.validateEmail(email6));
+		String email7 = "abc123@gmail.a";
+		System.out.println("EmailId of User:"+email7+"------->"+patternMatcher.validateEmail(email7));
+		String email8 = "abc123@com";
+		System.out.println("EmailId of User:"+email8+"------->"+patternMatcher.validateEmail(email8));
+		String email9 = ".abc@abc.com";
+		System.out.println("EmailId of User:"+email9+"------->"+patternMatcher.validateEmail(email9));
+		String email10 = "abc()*@gmail.com";
+		System.out.println("EmailId of User:"+email10+"------->"+patternMatcher.validateEmail(email10));
+		String email11 = "abc@%*.com";
+		System.out.println("EmailId of User:"+email11+"------->"+patternMatcher.validateEmail(email11));
+		String email12 = "abc..2002@gmail.com";
+		System.out.println("EmailId of User:"+email12+"------->"+patternMatcher.validateEmail(email12));
+		
 	}
+	
 	
 	
 	      
@@ -31,7 +57,7 @@ public class PatternMatcher {
 	    	  return "invalid";
 	      }
 	      private String validateEmail(String samplel) {
-	    	  String regex = "^(abc[.][A-Za-z]+@bl[.]co[.][A-Za-z]{3,25})$";
+	    	  String regex = "^[a-z]+[.|+_-]?[a-z0-9]+[@][a-z0-9]{1,}[.][a-z]{2,}[.]{0,1}[a-z]*$";
 	    	  Pattern pattern = Pattern.compile(regex);
 	    	  Matcher matcher = pattern.matcher(samplel);
 	    	  if(matcher.find()) {
